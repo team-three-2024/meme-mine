d2bb690b482450efbb1e5b5eb86977554539f36b
Vilson Vieira <vilson@void.cc>
2021-07-25 10:21:55 -0300

    Start using group of meshes instead of points for nodes

--- src/lib/ThreeCanary.js
+++ src/lib/ThreeCanary.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import * as THREE from "three";
+import { Vector3 } from "three";
 import { OBJLoader } from "three-obj-mtl-loader";
-// import OrbitControls from "three-orbitcontrols";
+import OrbitControls from "three-orbitcontrols";
 
 class ThreeCanary extends Component {
   constructor(props) {
@@ -17,14 +18,6 @@ class ThreeCanary extends Component {
     this.addLights();
     this.addMaterials();
     this.addModels();
-
-    // Add a cube to visualize intersection with pointer
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-    const cube = new THREE.Mesh( geometry, material );
-    this.cube = cube;
-    this.cube.scale.set(0.05, 0.05, 0.05);
-    this.scene.add( cube );
 
     this.renderScene();
     this.start();
@@ -47,18 +40,25 @@ class ThreeCanary extends Component {
 
   addCamera() {
     this.camera = new THREE.PerspectiveCamera(40, this.width / this.height, 1, 3000);
-    this.camera.position.z = 6;
-    this.camera.position.y = 2;
-    this.camera.position.x = -3;
+    this.camera.position.z = 30;
+    this.camera.position.y = 5;
   }
 
   addControls() {
-    // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     // Raycaster from camera to vertex pointer so we can interactive with 3D vertices
     this.pointer = new THREE.Vector2();
     this.raycaster = new THREE.Raycaster();
-    this.raycaster.params.Points.threshold = 0.05;
-    this.intersected = null;
+    // this.raycaster.params.Points.threshold = 2;
+    this.hoveredNodes = [];
+
+    // Add a cube to visualize intersection with pointer
+    // const geometry = new THREE.BoxGeometry();
+    // const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+    // const cube = new THREE.Mesh( geometry, material );
+    // this.cube = cube;
+    // this.cube.scale.set(0.05, 0.05, 0.05);
+    // this.scene.add( cube );
 
     // window.addEventListener("resize", this.onWindowResize);
     document.addEventListener("pointermove", this.onPointerMove);
@@ -69,9 +69,9 @@ class ThreeCanary extends Component {
     lights[0] = new THREE.PointLight(0xff00ff, 1, 0);
     lights[1] = new THREE.PointLight(0xffffff, 1, 0);
     lights[2] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[0].position.set(0, 200, 0);
-    lights[1].position.set(100, 200, 100);
-    lights[2].position.set(-100, -200, -100);
+    lights[0].position.set(0, 0, 0);
+    lights[1].position.set(0, 0, 0);
+    lights[2].position.set(0, 0, 0);
     this.scene.add(lights[0]);
     this.scene.add(lights[1]);
     this.scene.add(lights[2]);
@@ -80,10 +80,10 @@ class ThreeCanary extends Component {
   addMaterials() {
     // TODO: Move to brand color pallet
     this.canaryMtlMesh =  new THREE.PointsMaterial( { color: 0xe6007a });
-    this.canaryMtlPoints = new THREE.PointsMaterial( {
-      color: 0x8200f9,
-      size: 0.1
-    } );
+    // this.canaryMtlPoints = new THREE.PointsMaterial( {
+    //   color: 0x8200f9,
+    //   size: 0.1
+    // } );
   }
 
   addModels() {
@@ -108,13 +108,34 @@ class ThreeCanary extends Component {
 
             // Create point clouds based on mesh
             var childGeometry = child.geometry.clone();
-            this.canaryPointCloud = new THREE.Points(childGeometry, this.canaryMtlPoints);
-            this.canaryPointCloud.position.setY(-2);
-            this.canaryPointCloud.rotation.y = Math.PI/4;
-            this.canaryPointCloud.scale.set(4, 4, 4);
-            this.scene.add(this.canaryPointCloud);
+            // this.canaryPointCloud = new THREE.Points(childGeometry, this.canaryMtlPoints);
+            // this.canaryPointCloud.position.setY(-2);
+            // this.canaryPointCloud.rotation.y = Math.PI/4;
+            // this.canaryPointCloud.scale.set(4, 4, 4);
+            // this.scene.add(this.canaryPointCloud);
+            // let pos = this.canaryPointCloud.geometry.attributes.position;
 
-            console.log("Creating point cloud", this.canaryPointCloud);
+            // Create a group of meshes as a point cloud instead of points, to have
+            // per-mesh control
+            let pos = childGeometry.attributes.position;
+            this.canaryPointCloudGroup = new THREE.Group();
+            for (let i=0; i<pos.count; i+=1) {
+
+              let geometry = new THREE.BoxGeometry();
+              let material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+              material.wireframe = false;
+              material.needsUpdate = true;
+              let cube = new THREE.Mesh( geometry, material );
+
+              cube.position.copy(new Vector3(pos.getX(i), pos.getY(i), pos.getZ(i)));
+              let _scale = Math.random()*100;
+              cube.scale.set(_scale, _scale, _scale);
+              this.canaryPointCloudGroup.add( cube );
+            }
+            this.canaryPointCloudGroup.position.setY(-2);
+            this.canaryPointCloudGroup.rotation.y = Math.PI/4;
+            this.canaryPointCloudGroup.scale.set(4, 4, 4);
+            this.scene.add( this.canaryPointCloudGroup );
           }
         })
 
@@ -164,16 +185,33 @@ class ThreeCanary extends Component {
     
     this.raycaster.setFromCamera(this.pointer, this.camera);
 
-    if (this.canaryPointCloud) {
-      const intersects = this.raycaster.intersectObject(this.canaryPointCloud);
-      let intersection = ( intersects.length ) > 0 ? intersects[0] : null;
-      if (intersection !== null) {
-        console.log("intersection", intersection);
-        // intersection.object.material.color.set( 0xff0000 );
-        this.cube.position.copy(intersection.point);
-        // intersection.object.point.x;
+    this.hoveredNodes = [];
+    if (this.canaryPointCloudGroup) {
+      const intersects = this.raycaster.intersectObject(this.canaryPointCloudGroup, true);
+      if (intersects != null && intersects.length > 0) {
+        for (let i=0; i<intersects.length; i+=1) {
+          if (!this.hoveredNodes.includes(intersects[i].object.id))
+            this.hoveredNodes.push(intersects[i].object.id);
+        }
       }
 
+      for (let i=0; i<this.canaryPointCloudGroup.children.length; i+=1) {
+
+        if (this.hoveredNodes.includes(this.canaryPointCloudGroup.children[i].id)) {
+          this.canaryPointCloudGroup.children[i].material.color.set( 0xffffff );
+          this.canaryPointCloudGroup.children[i].material.wireframe = true;
+          this.canaryPointCloudGroup.children[i].scale.set(0.15, 0.15, 0.15);
+          this.canaryPointCloudGroup.children[i].rotateX(Math.sin(this.frameId / 70)/20);
+          this.canaryPointCloudGroup.children[i].rotateY(Math.sin(this.frameId / 100)/20);
+          this.canaryPointCloudGroup.children[i].rotateZ(Math.sin(this.frameId / 80)/20);
+      
+        } else {
+          this.canaryPointCloudGroup.children[i].material.color.set( 0x0000ff );
+          this.canaryPointCloudGroup.children[i].material.wireframe = false;
+          this.canaryPointCloudGroup.children[i].scale.set(0.05, 0.05, 0.05);
+        }
+        // this.canaryPointCloudGroup.children[i].position.y += Math.sin(this.frameId / 50) / 1000;
+      }
     }
 
     this.renderScene();
