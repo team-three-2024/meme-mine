cc35cbf6e16bdd3759d13b9b197f19c897761d61
Vilson Vieira <vilson@void.cc>
2021-09-27 07:31:01 -0300

    Increase number of test vertices

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nodesData: nodesDataFactory(50),
+      nodesData: nodesDataFactory(1500),
       nodeSelected: null
     };
   }
