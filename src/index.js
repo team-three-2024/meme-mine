ce2908f5b89063572da94c1581f043f73b25d5d0
Lauro Gripa <laurogripa@gmail.com>
2024-01-24 19:39:46 -0300

    Refactor index.js and remove unused code

--- src/index.js
+++ src/index.js
@@ -4,36 +4,9 @@ import { Game, defaultConfig } from './lib/Game'
 
 import './styles.css'
 
-// Utils
-
-const choose = choices => {
-  let index = Math.floor(Math.random() * choices.length)
-  return choices[index]
-}
-
-const nodesDataFactory = n => {
-  let data = []
-  for (let i = 0; i < n; i += 1) {
-    data.push({
-      id: Math.floor(Math.random() * 100),
-      name: choose(['Arthur C. Clarke', 'Douglas Adams', 'Isaac Asimov']),
-      level: choose(['human', 'cyborg']),
-      hash: choose(['0x08eded6a76d84e309e3f09705ea2853f', '0xdeadbeefe6a76d84e309e3f09705ea28589']),
-      img: choose(['/assets/t1.jpg', '/assets/t2.jpg'])
-    })
-  }
-  return data
-}
-
-// Example hosting component
-
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      nodesData: nodesDataFactory(150),
-      nodeSelected: null
-    }
   }
 
   render() {
@@ -48,24 +21,7 @@ class App extends React.Component {
             height: '100%'
           }}
         >
-          <Game
-            objectUrl={defaultConfig['canary'].objectUrl}
-            nodes={this.state.nodesData}
-            onNodeClick={nodeId => {
-              console.log('onNodeClick', nodeId)
-            }}
-            config={defaultConfig['canary']}
-          />
-
-          <div
-            className={'Info'}
-            style={{
-              padding: 10,
-              color: this.state.nodeSelected ? this.state.nodeSelected.color : '#ffffff'
-            }}
-          >
-            {this.state.nodeSelected ? this.state.nodeSelected.name : ''}
-          </div>
+          <Game objectUrl={defaultConfig['canary'].objectUrl} config={defaultConfig['canary']} />
         </div>
       </>
     )
