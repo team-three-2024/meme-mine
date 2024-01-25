4aabf3ac9d0a40d05ea4319edd86614b96a63c7b
Lauro Gripa <laurogripa@gmail.com>
2024-01-25 19:29:13 -0300

    Updates

---
+++ precache-manifest.572438efed95bbf65ef1fd26365d2c4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a010096e47ac1b58ee55",
+    "url": "/meme-mine/static/css/main.cf959df9.chunk.css"
+  },
+  {
+    "revision": "a010096e47ac1b58ee55",
+    "url": "/meme-mine/static/js/main.a010096e.chunk.js"
+  },
+  {
+    "revision": "26945f5cd79a7b2b1d81",
+    "url": "/meme-mine/static/js/1.26945f5c.chunk.js"
+  },
+  {
+    "revision": "2437c75546022d3a3b2f",
+    "url": "/meme-mine/static/js/runtime~main.2437c755.js"
+  },
+  {
+    "revision": "fd3f95b87091ad7191dff4a8a8785184",
+    "url": "/meme-mine/static/media/bidi.fd3f95b8.mjs"
+  },
+  {
+    "revision": "a045c5b818b9a5dbd1c0da85a9209292",
+    "url": "/meme-mine/static/media/index.a045c5b8.mjs"
+  },
+  {
+    "revision": "56365162ae520bd207a7ef9d91ff393b",
+    "url": "/meme-mine/index.html"
+  }
+];
