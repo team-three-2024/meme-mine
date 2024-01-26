2c314fbe627f6836b80f3d91576416d22664b777
Lauro Gripa <laurogripa@gmail.com>
2024-01-26 19:03:01 -0300

    Updates

---
+++ precache-manifest.3fbb36ab27aad3fa05d0d1675a15a3f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "30c054fa00ec56ba0a80",
+    "url": "/meme-mine/static/css/main.cf959df9.chunk.css"
+  },
+  {
+    "revision": "30c054fa00ec56ba0a80",
+    "url": "/meme-mine/static/js/main.30c054fa.chunk.js"
+  },
+  {
+    "revision": "b03b948586f53010cbcc",
+    "url": "/meme-mine/static/js/1.b03b9485.chunk.js"
+  },
+  {
+    "revision": "2437c75546022d3a3b2f",
+    "url": "/meme-mine/static/js/runtime~main.2437c755.js"
+  },
+  {
+    "revision": "fd3f95b87091ad7191dff4a8a8785184",
+    "url": "/meme-mine/static/media/bidi.fd3f95b8.mjs"
+  },
+  {
+    "revision": "a045c5b818b9a5dbd1c0da85a9209292",
+    "url": "/meme-mine/static/media/index.a045c5b8.mjs"
+  },
+  {
+    "revision": "654c3d69a5bf7a467cbc45d1ee6c758d",
+    "url": "/meme-mine/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62b16aefff97c3a4fdfc11ff0deb4350.js"
+  "/meme-mine/precache-manifest.3fbb36ab27aad3fa05d0d1675a15a3f2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/meme-mine/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
