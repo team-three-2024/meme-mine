d8f6b1814388458f529a71a17515bc1da7a28d73
Lauro Gripa <laurogripa@gmail.com>
2024-01-28 11:52:05 -0300

    Handle url prefix for different environments

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -9,4 +9,15 @@ function assetURL(path) {
   return `${assetPrefix}/assets/${path}`
 }
 
-export { assetURL }
+function prefix(path) {
+  const publicUrl = process.env.PUBLIC_URL
+
+  const isDevelopment = process.env.NODE_ENV === 'development'
+  const isRoot = publicUrl === '/'
+
+  const assetPrefix = isDevelopment || isRoot ? '' : process.env.PUBLIC_URL
+
+  return `${assetPrefix}/${path}`
+}
+
+export { assetURL, prefix }
--- src/lib/StartScreen.js
+++ src/lib/StartScreen.js
@@ -11,6 +11,7 @@ import { ProgressBar } from '../components/ProgressBar'
 import { usePreloadedVideos } from '../components/Videos'
 import { canaryConfig as config } from '../config'
 import { WebcamProvider } from '../context/GameContext'
+import { prefix } from '../helpers/url'
 
 const StartScreen = () => {
   const [showStartScreen, setShowStartScreen] = useState(true)
@@ -56,14 +57,14 @@ const StartScreen = () => {
   useEffect(() => {
     const loadModels = async () => {
       Promise.all([
-        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-        faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
-        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-        faceapi.nets.mtcnn.loadFromUri('/models'),
-        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-        faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models'),
-        faceapi.nets.ageGenderNet.loadFromUri('/models')
+        faceapi.nets.faceExpressionNet.loadFromUri(prefix('/models')),
+        faceapi.nets.faceRecognitionNet.loadFromUri(prefix('/models')),
+        faceapi.nets.ssdMobilenetv1.loadFromUri(prefix('/models')),
+        faceapi.nets.tinyFaceDetector.loadFromUri(prefix('/models')),
+        faceapi.nets.mtcnn.loadFromUri(prefix('/models')),
+        faceapi.nets.faceLandmark68Net.loadFromUri(prefix('/models')),
+        faceapi.nets.faceLandmark68TinyNet.loadFromUri(prefix('/models')),
+        faceapi.nets.ageGenderNet.loadFromUri(prefix('/models'))
       ])
     }
     loadModels()
