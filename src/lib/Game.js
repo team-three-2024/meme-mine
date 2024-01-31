10ae1bad0fce874b9ecdf9d4a2ab9372afec9d8f
Lauro Gripa <laurogripa@gmail.com>
2024-01-31 14:03:32 -0300

    Add initial delay of 1 second

--- src/lib/Game.js
+++ src/lib/Game.js
@@ -46,12 +46,12 @@ const Game = ({ videos }) => {
       setTimeout(() => {
         setIsGlitching(false)
 
-        const randomDelay = Math.random() * 5000
+        const randomDelay = 1000 + Math.random() * 5000
         setTimeout(glitchOut, randomDelay)
       }, 1000)
     }
 
-    const initialDelay = Math.random() * 5000
+    const initialDelay = 1000 + Math.random() * 7500
     const timeoutId = setTimeout(glitchOut, initialDelay)
 
     return () => clearTimeout(timeoutId)
